cc33cf5b2a036acc02a2835ecc4a146d22fb2386
Jana Pavlasek <jbpavlasek@gmail.com>
2017-02-16 11:57:15 -0500

    Small changes to make it run on my computer

--- scisvm.py
+++ scisvm.py
@@ -1,10 +1,10 @@
 from sklearn import svm
-from sklearn.model_selection import cross_val_score
+from sklearn.cross_validation import cross_val_score
 from clean_data import CleanData
 import numpy as np
 import csv
 
-cd = CleanData(tfidf=True,max_features=20000)
+cd = CleanData(tfidf=True, max_train_size=50000, max_features=8000)
 
 print 'Getting Training data.'
 training_data = cd.bag_of_words(in_file="data/clean_train_input.csv")
@@ -12,6 +12,8 @@ print 'Done collecting data.'
 
 X = [x[1] for x in training_data]
 y = [y[2] for y in training_data]
+
+del training_data
 
 print 'Training the model.'
 lin_clf = svm.LinearSVC()
@@ -28,37 +30,41 @@ test_data = cd.get_x_in()
 test = [x[1] for x in test_data]
 print 'Done collecting data.'
 
+del test_data
 
 print 'Predicting results.'
 out = lin_clf.predict(test)
 print 'Done predicitng.'
 
+del test, lin_clf, X, y, c_validation
+
 out = np.array(out).tolist()
 
-for i in range(0,len(out)):
-     if out[i] == 0:
-            out[i] = 'hockey'
-     if out[i] == 1:
-             out[i] = 'movies'
-     if out[i] == 2:
-             out[i] = 'nba'
-     if out[i] == 3:
-             out[i] = 'news'
-     if out[i] == 4:
-             out[i] = 'nfl'
-     if out[i] == 5:
-             out[i] = 'politics'
-     if out[i] == 6:
-             out[i] = 'soccer'
-     if out[i] == 7:
-             out[i] = 'worldnews'
+for i in range(0, len(out)):
+    if out[i] == 0:
+        out[i] = 'hockey'
+    if out[i] == 1:
+        out[i] = 'movies'
+    if out[i] == 2:
+        out[i] = 'nba'
+    if out[i] == 3:
+        out[i] = 'news'
+    if out[i] == 4:
+        out[i] = 'nfl'
+    if out[i] == 5:
+        out[i] = 'politics'
+    if out[i] == 6:
+        out[i] = 'soccer'
+    if out[i] == 7:
+        out[i] = 'worldnews'
 
 print out[0:5]
 
 print 'Writing results into CSV file'
-with open('svm_predictions.csv','wb') as result:
-     wr = csv.writer(result)
-     for item in out:
-         wr.writerow([item])
+with open('svm_predictions.csv', 'wb') as result:
+    wr = csv.writer(result)
+    wr.writerow(["id", "category"])
+    for i, item in enumerate(out):
+        wr.writerow([i, item])
 
 print 'Completed successfully'
